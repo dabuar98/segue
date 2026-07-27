d169b61c4465d1ee64f8a92cdf977f76bd50c4f1
Mauricio Bulla <dabuar98@gmail.com>
2026-07-27 07:54:32 -0500

    Uncomment genre dataset download pipeline

--- segue/scripts/get_genre_datasets.sh
+++ segue/scripts/get_genre_datasets.sh
@@ -4,13 +4,13 @@
 # Load and export all variables from the .env file
 export $(xargs < ../.env)
 
-## Download the datasets
-#echo "[ $(date '+%Y-%m-%d %H:%M:%S') ][ INFO ] GetGenreDatasets: Downloading discogs dataset"
-#wget -q https://zenodo.org/records/2553414/files/acousticbrainz-mediaeval-discogs-train.tsv.bz2?download=1 -O "${GENRE_DATASET_PATH}/discogs-train.tsv.bz2"
-#echo "[ $(date '+%Y-%m-%d %H:%M:%S') ][ INFO ] GetGenreDatasets: Downloading lastfm dataset"
-#wget -q https://zenodo.org/records/2553414/files/acousticbrainz-mediaeval-lastfm-train.tsv.bz2?download=1 -O "${GENRE_DATASET_PATH}/lastfm-train.tsv.bz2"
-#echo "[ $(date '+%Y-%m-%d %H:%M:%S') ][ INFO ] GetGenreDatasets: Downloading tagtraum dataset"
-#wget -q https://zenodo.org/records/2553414/files/acousticbrainz-mediaeval-tagtraum-train.tsv.bz2?download=1 -O "${GENRE_DATASET_PATH}/tagtraum-train.tsv.bz2"
+# Download the datasets
+echo "[ $(date '+%Y-%m-%d %H:%M:%S') ][ INFO ] GetGenreDatasets: Downloading discogs dataset"
+wget -q https://zenodo.org/records/2553414/files/acousticbrainz-mediaeval-discogs-train.tsv.bz2?download=1 -O "${GENRE_DATASET_PATH}/discogs-train.tsv.bz2"
+echo "[ $(date '+%Y-%m-%d %H:%M:%S') ][ INFO ] GetGenreDatasets: Downloading lastfm dataset"
+wget -q https://zenodo.org/records/2553414/files/acousticbrainz-mediaeval-lastfm-train.tsv.bz2?download=1 -O "${GENRE_DATASET_PATH}/lastfm-train.tsv.bz2"
+echo "[ $(date '+%Y-%m-%d %H:%M:%S') ][ INFO ] GetGenreDatasets: Downloading tagtraum dataset"
+wget -q https://zenodo.org/records/2553414/files/acousticbrainz-mediaeval-tagtraum-train.tsv.bz2?download=1 -O "${GENRE_DATASET_PATH}/tagtraum-train.tsv.bz2"
 
 # Extract files
 echo "[ $(date '+%Y-%m-%d %H:%M:%S') ][ INFO ] GetGenreDatasets: Extracting files"
