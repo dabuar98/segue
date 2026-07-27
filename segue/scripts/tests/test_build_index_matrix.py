13f9daf500b0039bbf266007ca2b69840ab567f2
Daniel Bulla <dabuar98@gmail.com>
2026-07-26 19:58:24 -0500

    Implement unit tests for build_index_matrix

---
+++ segue/scripts/tests/test_build_index_matrix.py
@@ -0,0 +1,32 @@
+"""
+Unit tests for build_index matrix. Check if script is returning a vector with all of its elements populated,
+using the correct data type. Run it from the project directory with py -m unittest -v scripts.tests.test_build_index_matrix
+"""
+
+
+import os
+import unittest
+import numpy as np
+from dotenv import load_dotenv
+from scripts.build_index_matrix import build_index_matrix
+
+# Define data path
+load_dotenv()
+DATA_PATH = os.getenv("DATA_PATH")
+input_path = f"{DATA_PATH}/tracks_min.json" # Take the min version for testing
+
+class TestBuildIndexMatrix(unittest.TestCase):
+    
+    def test_build_index_matrix(self):
+        result = build_index_matrix(input_path)
+        # Validate if size is correct
+        # Min track version has 4 tracks, then script should return a (4 x 231) matrix
+        self.assertEqual(result.shape, (4, 231))
+        # Check that type is float32
+        self.assertEqual(result.dtype, np.float32)
+        # Check that all elements are populated
+        self.assertFalse(np.isnan(result).any())    # This tests true if any of the elements in the array is still np.nan
+                                                    # Meaning the values are missing
+
+if __name__ == '__main__':
+    unittest.main()
