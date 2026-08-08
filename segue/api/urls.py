4ffb4ea08e6850cfb41df16d50d282bd2b8bae72
Daniel Bulla <dabuar98@gmail.com>
2026-08-08 01:43:01 -0500

    Add path to similarity

--- segue/api/urls.py
+++ segue/api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from views import similar
+from api.views import similar
 
 urlpatterns = [
     path('api/similar/', similar, name='similar')
