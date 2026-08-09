3fc7657395363aa3247b3ff501be37529c07809e
Daniel Bulla <dabuar98@gmail.com>
2026-08-09 18:46:08 -0500

    Import scaler and scale query vector before searching

--- segue/scripts/compute_similarity.py
+++ segue/scripts/compute_similarity.py
@@ -11,21 +11,28 @@ Returns:
 import os
 import sys
 from pathlib import Path
-
 import faiss
+import joblib
 from dotenv import load_dotenv
 
+# Execute it from /segue
 sys.path.append(str(Path(__file__).resolve().parent))
 from build_query_vector import build_query_vector
 
 load_dotenv()
 DATA_PATH = os.getenv("DATA_PATH")
-path_index = f"{DATA_PATH}/index.faiss"
 
 def compute_similarity(query, n=20):
     # Load index
-    faiss_index = faiss.read_index(path_index)
+    faiss_index = faiss.read_index(f"{DATA_PATH}/index.faiss")
+    # Load scaler
+    scaler = joblib.load(f"{DATA_PATH}/index_scaler.joblib")
     # Build query vector from track's audio descriptors
     query_vector = build_query_vector(query)
+
+    # Normalise query vector
+    query_vector = scaler.transform(query_vector)
+
+    # Search n-similar vectors
     distances, indices = faiss_index.search(query_vector, n)
     return distances, indices
