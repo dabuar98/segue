0bf831562e09d31408aceff6b5c2b5ac7597ac64
Daniel Bulla <dabuar98@gmail.com>
2026-08-17 15:28:19 -0500

    Serialise results using TrackSerialiser rather than displaying content from JSON.

--- segue/api/views.py
+++ segue/api/views.py
@@ -1,12 +1,13 @@
 import json
 import os
 import tempfile
-import numpy as np
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from dotenv import load_dotenv
 from scripts.compute_similarity import compute_similarity
+from app.models import *
+from .serialisers import TrackSerialiser
 
 """
 To run it curl -s -X POST http://localhost:8000/api/similar/ -F "audio=@data/sample.mp3" | python -m json.tool
@@ -31,21 +32,19 @@ def similar(request):
         for chunk in audio_file.chunks():
             tmp.write(chunk)
         tmp_path = tmp.name
-
     try:
         distances, indices = compute_similarity(tmp_path)
     finally:
         os.remove(tmp_path)
 
-    # Traverse both arrays at the same time (both have the same dimension)
-    for distance, index in zip(distances[0], indices[0]):
-        result.append({
-            'mbid': list(tracks.keys())[index],
-            'title': list(tracks.values())[index]['title'],
-            'artist': list(tracks.values())[index]['artist'],
-            'album': list(tracks.values())[index]['album'],
-            'subgenres': list(tracks.values())[index]['subgenres'],
-            'date': list(tracks.values())[index]['date'],
-            'distance': float(distance),
-        })
-    return JsonResponse(result, safe=False)
+    # Store database objects retrieved in a dict
+    tracks_objects = []
+
+    for index in indices[0]:
+        # Get mbid from tracks using index
+        mbid = list(tracks.keys())[index]
+        # Retrieve object from database using mbid and add it to tracks_objects
+        tracks_objects.append(Tracks.objects.get(mbid=mbid))
+
+    serialiser = TrackSerialiser(tracks_objects, many=True)
+    return JsonResponse(serialiser.data, safe=False)
