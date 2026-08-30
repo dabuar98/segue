2557f41b02174480c5ed3a7cd441e88b92255d6d
Mauricio Bulla <dabuar98@gmail.com>
2026-08-30 01:33:27 -0500

    Correct import

--- segue/scripts/build_query_vector.py
+++ segue/scripts/build_query_vector.py
@@ -6,7 +6,7 @@ Returns:
     result (nparray): A 1 x D Numpy array containing the values of the audio descriptors
 """
 import numpy as np
-from extract_query_descriptors import extract_query_descriptors
+from .extract_query_descriptors import extract_query_descriptors
 from datetime import datetime
 
 def map_key(key):
