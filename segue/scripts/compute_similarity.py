c0ad2e06b2932fd1e26e182808af47f3fbff79db
Mauricio Bulla <dabuar98@gmail.com>
2026-08-30 01:28:39 -0500

    Rewrite function to have query vector as input parameter instead of using path to file

--- segue/scripts/compute_similarity.py
+++ segue/scripts/compute_similarity.py
@@ -1,7 +1,7 @@
 """
 This script retrieves the N-most similar tracks to a query track
 Parameters:
-    - query (string): Path to the query track
+    - query_vector
     - n (int): The number of tracks to return (Default 20)
 Returns:
     - D (np.array): Distances
@@ -15,22 +15,18 @@ import faiss
 import joblib
 from dotenv import load_dotenv
 
-# Execute it from /segue
-sys.path.append(str(Path(__file__).resolve().parent))
-from build_query_vector import build_query_vector
-
 load_dotenv()
 DATA_PATH = os.getenv("DATA_PATH")
 
-def compute_similarity(query, n=20):
+def compute_similarity(query_vector, n=20):
+
     # Load index
     faiss_index = faiss.read_index(f"{DATA_PATH}/index.faiss")
+
     # Load scaler
     scaler = joblib.load(f"{DATA_PATH}/index_scaler.joblib")
-    # Build query vector from track's audio descriptors
-    query_vector = build_query_vector(query)
 
-    # Normalise query vector
+    # Apply scaler to query vector
     query_vector = scaler.transform(query_vector)
 
     # Search n-similar vectors
