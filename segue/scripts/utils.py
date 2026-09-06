7f654a1b24b7ac2092d36e809921025aa2a7c594
Daniel Bulla <dabuar98@gmail.com>
2026-09-05 20:54:49 -0500

    Change comment format

--- segue/scripts/utils.py
+++ segue/scripts/utils.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 def cosine_similarity(vect_A, vect_B):
-    '''
+    """
     Computes the cosine similarity between vect_A and vect_B
     @params vect_A, vect_B
     @returns similarity (float)
     Note: vect_A and vect_B must have the same dimension
-    '''
+    """
     # Calculate dot product and norms
     dot_product = np.dot(vect_A, vect_B)
     norm_A = np.linalg.norm(vect_A)
