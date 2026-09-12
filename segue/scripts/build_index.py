97c2f32017f76027cc31e75d93c0ac11d0d90185
Daniel Bulla <dabuar98@gmail.com>
2026-09-12 13:08:25 -0500

    Improve PyDoc and update script to compute Euclidean-distance-similarity using the matrix index for Tzanetakis

--- segue/scripts/build_index.py
+++ segue/scripts/build_index.py
@@ -1,27 +1,22 @@
-"""
-Generate the FAISS index used to compute similarity. Following Linden et al. [1], the index produced in this script
-is stored for offline similarity computation.
-Parameter:
-    - index_mat (string): Path to index_mat.npy
-    - n: Number of audio descriptors
-Returns: None
-References:
-    [1] Linden, G., Smith, B. and York, J. (2003)
-        'Amazon.com recommendations: Item-to-item collaborative filtering',
-        IEEE Internet Computing, 7(1), pp. 76–80. doi: 10.1109/MIC.2003.1167344.
-
-"""
 from datetime import datetime
 import faiss
 import numpy as np
-from pathlib import Path
 from dotenv import load_dotenv
 import os
 from sklearn.preprocessing import StandardScaler
 import joblib
 
-def build_index(index_mat, n):
-    index_mat = Path(index_mat)
+def build_index(index_mat):
+    """
+    Generate the FAISS index used to compute similarity. Following Linden et al. [1], the index produced in this script
+    is stored for offline similarity computation.
+    Args:
+        index_mat: Index matrix (NumPy binary)
+        n: Number of audio descriptors (integer)
+
+    Returns:
+        None
+    """
     # Load index matrix .npy
     raw_vectors = np.load(index_mat)
 
@@ -30,11 +25,12 @@ def build_index(index_mat, n):
     vectors = scaler.fit_transform(raw_vectors)
 
     # Normalise vectors
-    faiss.normalize_L2(vectors)
+    # faiss.normalize_L2(vectors)
 
     # Build index
     print(f"[ {datetime.now():%Y-%m-%d %H:%M:%S} ][ INFO ] BuildIndex: Start building faiss index")
-    faiss_index = faiss.IndexFlatIP(n)
+    # faiss_index = faiss.IndexFlatIP(n)
+    faiss_index = faiss.IndexFlat(vectors.shape[1])
 
     # Add vectors to index
     faiss_index.add(vectors)
@@ -52,6 +48,5 @@ def build_index(index_mat, n):
 if __name__ == "__main__":
     load_dotenv()
     DATA_PATH = os.getenv("DATA_PATH")
-    path_index = f"{DATA_PATH}/index_mat.npy"
-    build_index(path_index, 75)
+    build_index(f"{DATA_PATH}/index_mat_tzanetakis.npy")
 
