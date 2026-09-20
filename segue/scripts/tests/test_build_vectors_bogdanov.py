81a03d9557ae32544c247553c51fd458a0e0b339
Daniel Bulla <dabuar98@gmail.com>
2026-09-19 19:34:15 -0500

    Update dimension

--- segue/scripts/tests/test_build_vectors_bogdanov.py
+++ segue/scripts/tests/test_build_vectors_bogdanov.py
@@ -25,11 +25,15 @@ class TestBuildVectorsBogdanov(unittest.TestCase):
             data = json.load(f) # Transform to a Python dict
 
         result = build_index_vector_bogdanov(data) # Receives a Python dict
-        # Test dimension: bark bands (27) + pitch (2) + spectral centroid/spread/kurtosis/rolloff/decrease/
-        # skewness (12) + HFC (2) + spectral complexity (2) + crest/flatness (4) + flux (2) + spectral energy/
-        # energy bands/strong peak (12) + beats loudness (2) + beats loudness bass (2) + untransposed HPCP (36)
-        # + key strength (1) + average loudness (1) + zero-crossing rate (2) = 107
-        self.assertEqual(result.shape, (1, 107))
+        # Test dimension: bark bands (27) + pitch (2) + MFCC mean (13) + MFCC covariance diagonal (13) + spectral
+        # centroid/spread/kurtosis/rolloff/decrease/skewness (12) + HFC (2) + spectral complexity (2) +
+        # crest/flatness (4) + flux (2) + spectral energy/energy bands/strong peak (12) + BPM histogram peaks (6)
+        # + beats loudness (2) + beats loudness bass (2) + untransposed HPCP (36) + transposed HPCP (36) + key
+        # strength (1) + tuning frequency (1) + dissonance (2) + chord change rate (1) + chords histogram (24) +
+        # chords strength (2) + tuning equal tempered deviation (1) + tuning non-tempered energy ratio (1) +
+        # tuning diatonic strength (1) + average loudness (1) + zero-crossing rate (2) + silence rate (3) +
+        # spectral RMS variance (1) = 212
+        self.assertEqual(result.shape, (1, 212))
         # Script should return a np array of type float32
         self.assertIsInstance(result, np.ndarray)
         self.assertEqual(result.dtype, np.float32)
