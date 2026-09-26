ae415b3949d7257e1be1ed8f6630a09b96c9ac4f
Daniel Bulla <dabuar98@gmail.com>
2026-09-26 00:06:33 -0500

    Add paths to new request handlers

--- segue/api/urls.py
+++ segue/api/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
 from django.urls import path
-from api.views import similar
+from api.views import create_query, query_status, similar
 
 urlpatterns = [
-    path('similar/', similar, name='similar')
+    path('queries/', create_query, name='create_query'),
+    path('queries/<uuid:query_id>/', query_status, name='query_status'),
+    path('queries/<uuid:query_id>/similar/', similar, name='similar')
 ]
